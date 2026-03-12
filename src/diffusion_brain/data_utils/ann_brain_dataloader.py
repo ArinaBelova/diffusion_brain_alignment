WIP: removed distributed training fully, instead having gradient checkpointing, bfloat16 during trianing  and gradient accummulation. Fixing the issue of "empty conditioning" by having a non-zero condition vector in generation and adding cross-attention blocks to UNet2D.

src/diffusion_brain/data_utils/ann_brain_dataloader.py
@@ -3,7 +3,6 @@
 # and annotation is the corresponding activations from layer of ANN, given the coco image
 
 from torch.utils.data import DataLoader, TensorDataset, Dataset
-from torch.utils.data.distributed import DistributedSampler
 import torch 
 import numpy as np
 import os
@@ -11,18 +10,6 @@ import os
 from diffusion_brain.datasets.paired_brain_ann_dataset import AnnActivationsDataset, PairedBrainAnnDataset
 from diffusion_brain.utils.ann_activations_utils import ensure_activations_exist
 from diffusion_brain.utils.fmri_behav_data_utils import get_train_test_subsets, get_train_test_indices, ensure_fmri_roi_exists
-
-def _build_train_sampler(dataset, args):
-    distributed = getattr(args, "distributed", None)
-    if distributed is None or not getattr(distributed, "is_distributed", False):
-        return None
-    return DistributedSampler(
-        dataset,
-        num_replicas=distributed.world_size,
-        rank=distributed.rank,
-        shuffle=True,
-        drop_last=True,
-    )
 
 class ZipDataset(Dataset):
     def __init__(self, *datasets):
@@ -96,12 +83,10 @@ def get_ann_brain_dataloader(args):
     if args.state != "train":
         return test_dataloader
     
-    train_sampler = _build_train_sampler(train_dataset, args)
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=args.train.batch_size,
-        shuffle=train_sampler is None,
-        sampler=train_sampler,
+        shuffle=True,
         num_workers=args.train.num_workers,
         drop_last=True
     )

src/diffusion_brain/data_utils/mnist_dataloader.py
@@ -3,7 +3,6 @@ import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
-from torch.utils.data.distributed import DistributedSampler
 import diffusion_brain.utils.diffusivity as diffusivity
 
 def get_mnist_dataloader(args):
@@ -22,19 +21,8 @@ def get_mnist_dataloader(args):
     train_batch_size = args.train.batch_size if hasattr(args.train, 'batch_size') else 64
     val_batch_size = args.validation.batch_size if hasattr(args.validation, 'batch_size') else 1
 
-    distributed = getattr(args, "distributed", None)
-    train_sampler = None
-    if distributed is not None and getattr(distributed, "is_distributed", False):
-        train_sampler = DistributedSampler(
-            train_set,
-            num_replicas=distributed.world_size,
-            rank=distributed.rank,
-            shuffle=True,
-            drop_last=False,
-        )
-
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=train_batch_size,
-                                              shuffle=train_sampler is None, sampler=train_sampler, num_workers=2)
+                                              shuffle=True, num_workers=2)
     val_loader = torch.utils.data.DataLoader(val_set, batch_size=val_batch_size,
                                               shuffle=True, num_workers=2)
 

src/diffusion_brain/data_utils/toy_dataloader.py
@@ -1,7 +1,6 @@
 import torch
 import numpy as np
 from torch.utils.data import Dataset
-from torch.utils.data.distributed import DistributedSampler
 
 def get_toy_dataloader(args):
     dataset = EightGaussianConditional(
@@ -11,21 +10,10 @@ def get_toy_dataloader(args):
         label_type=args.train.label_type,  # 'index' or 'coordinates'
         radius=args.data.radius,
     )
-    distributed = getattr(args, "distributed", None)
-    sampler = None
-    if distributed is not None and getattr(distributed, "is_distributed", False):
-        sampler = DistributedSampler(
-            dataset,
-            num_replicas=distributed.world_size,
-            rank=distributed.rank,
-            shuffle=True,
-            drop_last=False,
-        )
     dataloader = torch.utils.data.DataLoader(
         dataset,
         batch_size=args.train.batch_size,
-        shuffle=sampler is None,
-        sampler=sampler,
+        shuffle=True,
     )
     return dataloader, None
 

src/diffusion_brain/utils/diffusivity.py
@@ -172,23 +172,28 @@ def run_reverse_sde(diffusion_process: StandardDiffusion,
         #     else:
         #         score = score_fn(x, t, y_target) / torch.sqrt(diffusion_process.var(t))
         # else:
-        # for mnist:
+        # for mnist and ann-brain with UNet2DConditionModel:
         if args.model.name == "unet-diffusers" or args.model.name == "unet-diffusers-1d":
 
             #######################
             time_unet = t * 999
             #######################
             
-            encoder_hidden_states = torch.zeros(x.shape[0], 1, args.model.cross_attention_dim, device=x.device)
-
-            # print("y_empty:", y_empty.device, y_empty.shape, y_empty.dtype)
-            # print("y_target:", y_target.device, y_target.shape, y_target.dtype)
-            # print("x: ", x.device, x.shape, x.dtype)
-            # print("t: ", t.device, t.shape, t.dtype)
-            # print("encoder_hidden_states: ", encoder_hidden_states.device, encoder_hidden_states.shape, encoder_hidden_states.dtype)
-
-            score_uncond = score_fn(x, time_unet, encoder_hidden_states = encoder_hidden_states, class_labels=y_empty).sample
-            score_cond = score_fn(x, time_unet, encoder_hidden_states = encoder_hidden_states, class_labels=y_target).sample
+            cond_seq_len = getattr(args.model, "cond_seq_len", 4)
+            
+            # For ann-brain: use actual conditioning via cross-attention
+            if cond is not None:
+                # cond shape: (batch, cond_dim) -> (batch, seq_len, cond_dim)
+                encoder_hidden_states_cond = cond.unsqueeze(1).expand(-1, cond_seq_len, -1).float()
+                encoder_hidden_states_uncond = torch.zeros_like(encoder_hidden_states_cond)
+                
+                score_uncond = score_fn(x, time_unet, encoder_hidden_states=encoder_hidden_states_uncond, class_labels=None).sample
+                score_cond = score_fn(x, time_unet, encoder_hidden_states=encoder_hidden_states_cond, class_labels=None).sample
+            else:
+                # For mnist: use class_labels (discrete)
+                encoder_hidden_states = torch.zeros(x.shape[0], cond_seq_len, args.model.cross_attention_dim, device=x.device)
+                score_uncond = score_fn(x, time_unet, encoder_hidden_states=encoder_hidden_states, class_labels=y_empty).sample
+                score_cond = score_fn(x, time_unet, encoder_hidden_states=encoder_hidden_states, class_labels=y_target).sample
         # for ann-brain case with continuous conditioning vector:
         elif args.model.name == "gfdm-unet-1d-cond" or args.model.name == "dit":
             #######################
@@ -243,6 +248,10 @@ def generate_samples(num_samples: int,
     """Function to generate samples from the learned diffusion model"""
     # initial samples from p_T
     raw_model = _unwrap_model(model)
+    
+    # Track original size for cropping 2D data back
+    original_size = getattr(args.model, "input_size_original", None)
+    
     if args.data.data_name == "toy":
         dim_x = [args.model.input_size]
     elif args.model.name == "gfdm-unet-1d-cond":
@@ -259,7 +268,8 @@ def generate_samples(num_samples: int,
     elif args.data.data_name == "mnist": 
         dim_x = (args.model.c_in, args.model.input_size, args.model.input_size)
     else:
-        dim_x = args.model.input_size # for rectangular shapes
+        # For 2D brain data: input_size is already the padded size (C, H_padded, W_padded)
+        dim_x = args.model.input_size
 
     noise = torch.randn(size=(num_samples, *dim_x), device=device)
     mu, std = diffusion_process.brown_moments(torch.zeros(num_samples, *dim_x).to(device), diffusion_process.T)
@@ -287,6 +297,12 @@ def generate_samples(num_samples: int,
 
     if args.model.name == "gfdm-unet-1d-cond":
         x_0 = x_0[..., :orig_len]
+    
+    # Crop 2D brain data back to original size
+    is_2d = getattr(args.data, "is_2d", False)
+    if is_2d and original_size is not None and len(original_size) == 3:
+        _, orig_h, orig_w = original_size
+        x_0 = x_0[..., :orig_h, :orig_w]
 
     # print("x_0 and label shapes are: ", x_0.shape, cond.shape, flush=True)    
     return x_0 # * 255 as I don't really know what scale the model learned...

src/diffusion_brain/utils/visualise.py
@@ -5,11 +5,7 @@ import wandb
 from matplotlib import pyplot as plt
 import numpy as np
 import cortex
-import plotly
-from sklearn.metrics import r2_score
 import scipy
-from diffusion_brain.utils.fmri_behav_data_utils import signal_to_2d
-from diffusion_brain.utils.fmri_behav_data_utils import get_train_test_indices
 
 def _attach_model_step(payload, step_num):
     if step_num is None:
@@ -37,6 +33,9 @@ def visualise_and_save_results(generated_samples, true_fmri, step, args, step_nu
         ax.set_ylim(-args.data.radius - 2, args.data.radius + 2)
         wandb.log(_attach_model_step({"validation_sample": wandb.Image(fig)}, step_num))
         plt.close(fig)
+
+        return -float('inf') # for toy data we don't calculate r scores, as they are not informative for this type of data
+    
     elif args.data.data_name == "ann-brain":
         # for now I don't check the visual quality of generated samples, so I don't want to use this function
         # if isinstance(generated_samples, torch.Tensor):
@@ -52,6 +51,9 @@ def visualise_and_save_results(generated_samples, true_fmri, step, args, step_nu
         else:
             r_scores_across_batch_images = get_r_across_images_1d_data(args, generated_samples, true_fmri, step_num=step_num)
             pyplot_brain(r_scores_across_batch_images, args=args, savename=f"r_scores_across_batch_images_step", figpath=f"{args.validation.output_folder}/{args.jobid}", save_type='png', step_num=step_num)
+            r_scores_across_batch_images = np.mean(r_scores_across_batch_images)
+            
+        return r_scores_across_batch_images    
 
     else:  
         #generated_samples = generated_samples.cpu().numpy()  
@@ -64,6 +66,8 @@ def visualise_and_save_results(generated_samples, true_fmri, step, args, step_nu
         if not os.path.exists(directory_to_save):
             os.makedirs(directory_to_save)
         torchvision.utils.save_image(generated_samples, f"{directory_to_save}/generated_samples_step_{step}.png", nrow=int(np.sqrt(args.validation.batch_size)))
+
+        return -float('inf')
 
 # Function is courtesy of https://github.com/adriendoerig/visuo_llm/blob/main/src/nsd_visuo_semantics/utils/py_plot_brain_utils.py
 def pyplot_brain(fsavg_data, savename, figpath, args, save_type='png', max_cmap_val=None, step_num=None):
@@ -150,15 +154,15 @@ def get_r_across_images_2d_data(args, generated_data_roi_2d, true_fmri, step_num
 
     print("Calculating r scores across voxels...", flush=True)
     r_scores_across_voxels = np.empty((time_series.shape[0],))
-    r2_scores_across_batch_images = np.empty((time_series.shape[1],))
+    r_scores_across_batch_images = np.empty((time_series.shape[1],))
     print("Shape of r_scores_across_voxels array:", r_scores_across_voxels.shape)  # Should be (num_images,)
 
-    print("Calculating r2 scores across batch images...", flush=True)
+    print("Calculating r scores across batch images...", flush=True)
     for voxel_idx in range(time_series.shape[1]):
         true_fmri_by_image = true_fmri[:,voxel_idx]
         generated_sample = time_series[:,voxel_idx]
         assert len(true_fmri_by_image) == len(generated_sample)
-        r2_scores_across_batch_images[voxel_idx] = scipy.stats.pearsonr(true_fmri_by_image, generated_sample)[0]
+        r_scores_across_batch_images[voxel_idx] = scipy.stats.pearsonr(true_fmri_by_image, generated_sample)[0]
 
     print("Calculating r scores across voxels...", flush=True)
     for image_idx in range(time_series.shape[0]):
@@ -169,10 +173,12 @@ def get_r_across_images_2d_data(args, generated_data_roi_2d, true_fmri, step_num
 
     r_payload = _attach_model_step({"r_image": wandb.Image(fig),
                                     "mean_r_scores_across_voxels": np.mean(r_scores_across_voxels),
-                                    "mean_r2_scores_across_batch_images": np.mean(r2_scores_across_batch_images)}, step_num)
+                                    "mean_r_scores_across_batch_images": np.mean(r_scores_across_batch_images)}, step_num)
     wandb.log(r_payload)
 
     plt.close(fig)    
+
+    return np.mean(r_scores_across_batch_images) # important for saving the best running model based on this metric 
 
 def get_r_across_images_1d_data(args, generated_data_roi, true_fmri, step_num=None, **kwargs):
      # to store r scores across images for each voxel
@@ -204,5 +210,7 @@ def get_r_across_images_1d_data(args, generated_data_roi, true_fmri, step_num=No
     }, step_num)
 
     wandb.log(r_payload)
+
+    return r_scores_across_batch_images #np.mean(r_scores_across_batch_images)
 
                     